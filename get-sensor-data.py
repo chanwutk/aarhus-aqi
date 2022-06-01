c38bc3440184f8a5e0b3424319dc95393a2e9aa2
nussara tieanklin <nussara@uw.edu>
2022-06-01 12:47:19 +0200

    hid token

--- get-sensor-data.py
+++ get-sensor-data.py
@@ -1,23 +1,39 @@
 import requests
 import json
 
+from .token import token
+
 def write_data(file, response):
     with open(file, "w") as f:
         f.write(response.text)
 
 def get_sensor_data(start, end): 
 
+    # TODO get the for loop 
     url = "https://api.cityflow.live/measurements/history/location/252"
 
     querystring = {"from": start,"to": end}
 
     payload = ""
-    headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTI0LCJpYXQiOjE2MTk2MzAwMzMsImV4cCI6MTkwOTcxNjQzM30.o2XZJEE9RE71Z-2z8oYLYD-9QANbi-fF1iTRvroTrx0'}
+
+    #TODO hidden bearer token 
+    headers = {'Authorization': token}
 
     response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
     return response
 
 def get_devices_locations():
+
+    # open all the devices file
+    f = open('all-devices.json')
+    data = json.loads(f)
+
+    # TODO: filter only from Aarhus
+    filtered = 
+
+    # close file
+    f.close()
+
 
 
 
@@ -26,5 +42,5 @@ if __name__ == '__main__':
     start = "2022-04-29T00:00:00.000Z"
     end = "2022-05-23T23:59:59.999Z"
 
-    response = get_sensor_data(start, end)
-    write_data("sensor-data.json", response)
+    #response = get_sensor_data(start, end)
+    #write_data("sensor-data.json", response)
