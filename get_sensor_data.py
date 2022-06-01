42b9e2da7a5c654decc9dff7658357a45d12805d
nussara tieanklin <nussara@uw.edu>
2022-06-01 15:44:35 +0200

    merged measurement and sensor info

---
+++ get_sensor_data.py
@@ -0,0 +1,55 @@
+import requests
+import json
+
+from bearer_token import token
+
+def write_data(file, response):
+    with open(file, "w", encoding='utf8') as f:
+        json.dump(response, f, ensure_ascii=False)
+
+def get_sensor_data(start, end, resolution, location): 
+
+    url = "https://api.cityflow.live/measurements/history/location/" + str(location)
+
+    querystring = {"from": start,"to": end, "resolution": resolution}
+    payload = ""
+
+    headers = {'Authorization': token}
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+    return [{**r, 'location': location} for r in response.json()]
+
+def get_devices():
+
+    # open all the devices file
+    with open('all-devices.json', 'r') as f:
+        data = json.loads(f.read())
+
+    devices = [d for d in data if d['city'].lower().find('aarhus') >= 0]
+    write_data('aarhus-devices.json', devices)
+
+    return devices
+
+if __name__ == '__main__':
+    
+    start = "2022-04-29T00:00:00.000Z"
+    end = "2022-05-23T23:59:59.999Z"
+    resolution = "60m"
+
+    ## run this if the file has never been generated
+    #devices = get_devices()
+    
+    # else this
+    with open('aarhus-devices.json', 'r') as f:
+        a_devices = json.loads(f.read())
+
+    location_list = [d['location'] for d in a_devices]
+    response = []
+
+    for location in location_list:
+        r = get_sensor_data(start, end, resolution, location)
+        response += r
+    
+    write_data("sensor-data.json", response)
+
+
---
+++ merge-data.py
@@ -0,0 +1,24 @@
+import json 
+from get_sensor_data import write_data
+
+# open files
+with open('aarhus-devices.json', 'r') as f:
+    devices = json.loads(f.read())
+
+with open('sensor-data.json', 'r') as f:
+    measurements = json.loads(f.read())
+
+merged_data = []
+
+for measurement in measurements:
+    for device in devices:
+        if measurement['location'] == device['location']:
+            merged_data.append(
+                {
+                    **measurement,
+                    **device
+                }
+            )
+            break
+
+write_data("sensor_measurement.json", merged_data)
