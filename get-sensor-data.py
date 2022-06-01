d966c31a2a5d22eeed12ec016acbe3c0e0b2382c
nussara tieanklin <nussara@uw.edu>
2022-06-01 11:27:05 +0200

    get data from 1 location

--- get-sensor-data.py
+++ get-sensor-data.py
@@ -1,23 +1,30 @@
 import requests
+import json
 
- 
+def write_data(file, response):
+    with open(file, "w") as f:
+        f.write(response.text)
 
-url = "https://api.cityflow.live/measurements/history/location/252"
+def get_sensor_data(start, end): 
 
- 
+    url = "https://api.cityflow.live/measurements/history/location/252"
 
-querystring = {"from":"2022-04-30T00:00:00.000Z","to":"2022-05-23T23:59:59.999Z"}
+    querystring = {"from": start,"to": end}
 
- 
+    payload = ""
+    headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTI0LCJpYXQiOjE2MTk2MzAwMzMsImV4cCI6MTkwOTcxNjQzM30.o2XZJEE9RE71Z-2z8oYLYD-9QANbi-fF1iTRvroTrx0'}
 
-payload = ""
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+    return response
 
-headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTI0LCJpYXQiOjE2MTk2MzAwMzMsImV4cCI6MTkwOTcxNjQzM30.o2XZJEE9RE71Z-2z8oYLYD-9QANbi-fF1iTRvroTrx0'}
+def get_devices_locations():
 
- 
 
-response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
 
- 
+if __name__ == '__main__':
+    
+    start = "2022-04-29T00:00:00.000Z"
+    end = "2022-05-23T23:59:59.999Z"
 
-print(response.text)
+    response = get_sensor_data(start, end)
+    write_data("sensor-data.json", response)
